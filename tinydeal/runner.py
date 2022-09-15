a78eb3e8425b778dce7e642d7f08fe648a2953da
George Marchenko <george.marchenko44@gmail.com>
2022-09-15 00:15:53 -0400

    scraping data from tinydeal

---
+++ tinydeal/runner.py
@@ -0,0 +1,9 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from tinydeal.spiders.special_offers import SpecialOffersSpider
+
+
+process = CrawlerProcess(settings=get_project_settings())
+process.crawl(SpecialOffersSpider)
+process.start()
